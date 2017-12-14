0d9dd449989c533e6e029d71ac958bb76e2dd0ec
V-Sergeev <sergeev.valer@gmail.com>
2017-12-14 16:58:44 +0700

    change task1

--- task_1/sumOfTwo.js
+++ task_1/sumOfTwo.js
@@ -1,8 +1,27 @@
-function sumOfTwo(array1, array2, x) {
+/*function sumOfTwo(array1, array2, x) {
     return array1.some(function(item1){
       return array2.some(function(item2){
         return item1+item2==x;
       })
     })
     }
-    
+*/
+
+function sumOfTwo(array1, array2, x) {
+    var keys={};//Набор уникальных значений 
+   //приведение массива к ассоциативному
+    function arrayToKeys(arr){
+    arr.forEach(function(element, index) {
+        keys[element]=index;});
+     }
+  //проверка на длину 
+   if(array1.length>array2.length) {
+     arrayToKeys(array1);
+   } else {
+     arrayToKeys(array2);
+   }
+   return array2.some(function(item2) {
+    return keys.hasOwnProperty(x-item2);
+   });
+}
+   
